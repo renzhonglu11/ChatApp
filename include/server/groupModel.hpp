3cf08aae97bd66737d194db68e013a93272b5638
renzhong <lu631992923q@gmail.com>
2025-11-16 21:11:09 +0100

    add group feature

---
+++ include/server/groupModel.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "group.hpp"
+#include <string>
+#include <vector>
+
+class GroupModel
+{
+public:
+    bool createGroup(Group &group);
+    void addGroup(int userId, int groupId, const std::string &role);
+    std::vector<Group> queryGroups(int userId);                // Check the group info that the user has joined
+    std::vector<int> queryGroupUsers(int groupId, int userId); // Check all users in the group except userId
+};
