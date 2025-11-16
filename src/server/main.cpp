0578d3b73f989d6aec7abc4ec46f32d5b9251b4d
renzhong <lu631992923q@gmail.com>
2025-11-16 20:18:58 +0100

    handle interrupt in server side

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,20 @@
 #include "chatserver.hpp"
 #include <iostream>
+#include <signal.h>
+#include "chatservice.hpp"
+
+void resetHandler(int)
+{
+    std::cout << "\nServer is shutting down. Resetting all user states..." << std::endl;
+    ChatService::instance()->reset();
+    exit(0);
+}
 
 int main()
 {
+
+    signal(SIGINT, resetHandler);
+
     EventLoop loop;
     InetAddress addr("127.0.0.1", 8888);
     ChatServer server(&loop, addr, "ChatServer");
